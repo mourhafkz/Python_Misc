3b0fe39aee3d6abffd62942d9e1efa8536a20db8
mourhafkz <mourhafkazzaz@gmail.com>
2020-10-31 15:45:00 +0530

    Create help1.py

---
+++ help1.py
@@ -0,0 +1,18 @@
+# any type of search and replace is easier with dictionary
+# check the temp_list result and tell me what your desired input is 
+
+
+word = input('write your word here:')
+replacement_dict = {'a': '1', 'e': '2', 'i': '3', 'o': '4', 'u': '5'}
+temp_list = []
+
+for letter in word:
+    if letter in replacement_dict:
+        word.replace(letter, replacement_dict[letter])
+        temp_list.append(replacement_dict[letter])
+    else:
+        word.replace(letter, letter+'a')
+        temp_list.append(letter)
+
+print(temp_list)
+print(word)
