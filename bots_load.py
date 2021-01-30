82fe2e1720d139b498bad9a6b6ccd02905efe2f3
mourhafkz <mourhafkazzaz@gmail.com>
2021-01-30 17:08:16 +0530

    Create bots_load.py

---
+++ bots_load.py
@@ -0,0 +1,39 @@
+import glob
+import os
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+
+"""
+expect data to be in /data/en/
+"""
+# print(os.getcwd())
+def iter_docs(author):
+    author_attr = author.attrib
+    doc_dict = author_attr.copy()
+    #    print(doc_dict)
+    doc_dict['text'] = [' '.join([doc.text for doc in author.iter('document')])]
+
+    return doc_dict
+
+
+def create_data_frame(input_folder):
+    os.chdir(input_folder)
+    all_xml_files = glob.glob("*.xml")
+    truth_data = pd.read_csv('truth.txt', sep=':::', names=['author_id', 'author', 'gender'], engine= "python")
+
+    temp_list_of_DataFrames = []
+    text_Data = pd.DataFrame()
+    for file in all_xml_files:
+        etree = ET.parse(file)  # create an ElementTree object
+        doc_df = pd.DataFrame(iter_docs(etree.getroot()))
+        doc_df['author_id'] = file[:-4]
+        temp_list_of_DataFrames.append(doc_df)
+    text_Data = pd.concat(temp_list_of_DataFrames, axis=0)
+
+    data = text_Data.merge(truth_data, on='author_id')
+    return data
+
+
+data = create_data_frame('data/en/')
+print(data)
